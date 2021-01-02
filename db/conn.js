f276db2633fd4b31b97575b9dd81fc4602d9cc3a
Xueshan Bao <Xueshan.bao@gmail.com>
2021-01-02 05:44:47 -0500

    fighting the heroku database bug

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ const conn = new Sequelize('chat_app_db', 'postgres', 'dawson', {
   host: 'db',
   dialect: 'postgres'
 });
-*/
+
 let conn
 if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
@@ -21,5 +21,19 @@ if (process.env.DATABASE_URL) {
     dialect: 'postgres'
   });
 }
+*/
+var database = process.env.DATABASE_URL || 'campeonatodb'
+var conn = ""
+console.log(database)
+if (process.env.DATABASE_URL) {
+    conn = new Sequelize(database)
+}
+else {
+  conn = new Sequelize('chat_app_db', 'postgres', 'dawson', {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+}
+
 
 module.exports = conn;
